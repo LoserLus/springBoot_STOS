6040fde005791c73c6f504a4c0bdc589dc0df255
SuDejie <2734196096@qq.com>
2022-07-03 22:24:26 +0800

    session-bug修复

--- src/main/java/com/sdust/stos/config/WebMvcConfig.java
+++ src/main/java/com/sdust/stos/config/WebMvcConfig.java
@@ -1,11 +1,20 @@
 package com.sdust.stos.config;
 
+
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
+@Slf4j
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
