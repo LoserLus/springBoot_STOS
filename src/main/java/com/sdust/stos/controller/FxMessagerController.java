2b26269b447c982ae067592c4cfcfa008d126c92
SuDejie <2734196096@qq.com>
2022-07-03 16:19:18 +0800

    实现了获取订购单，隐藏了配置文件

--- src/main/java/com/sdust/stos/controller/FxMessagerController.java
+++ src/main/java/com/sdust/stos/controller/FxMessagerController.java
@@ -9,15 +9,19 @@ import com.sdust.stos.entity.FxMessager;
 import com.sdust.stos.entity.TextMessage;
 import com.sdust.stos.service.DgListService;
 import com.sdust.stos.service.FxMessagerService;
+import com.sdust.stos.service.TextMessageService;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @Slf4j
@@ -30,6 +34,9 @@ public class FxMessagerController {
 
     @Autowired
     private DgListService dgListService;
+
+    @Autowired
+    private TextMessageService textMessageService;
 
     /**
      * 获取用户订购书籍列表
@@ -47,19 +54,30 @@ public class FxMessagerController {
         //获取订购列表
         List<DgList> dgList = dgListService.list(queryWrapper);
 
-        //在订购列表中加入对应书号的名称
-        //
+        //通过stream流遍历dgList,
+        List<DgListDto> dgListDto = dgList.stream().map((item)->{
 
-        List<DgListDto> dgListDto = null;
+            //创建一个dgListDto
+            DgListDto dgListDto1 = new DgListDto();
 
-        for(int i=0;i<dgList.size();i++){
+            //copy dgList到dgListDto
+            BeanUtils.copyProperties(item,dgListDto1);
 
-        }
+            //获取这条订购记录的书号
+            String isbn = item.getIsbn();
 
+            TextMessage textMessage = textMessageService.getById(isbn);
 
+            //设置书的名称
+            if(textMessage != null){
+                String bookName = textMessage.getBookName();
+                dgListDto1.setBookName(bookName);
+            }
 
+            return dgListDto1;
+        }).collect(Collectors.toList());
 
-        return null;
+        return R.success(dgListDto);
     }
 
 }
--- src/main/java/com/sdust/stos/dto/DgListDto.java
+++ src/main/java/com/sdust/stos/dto/DgListDto.java
@@ -2,11 +2,13 @@ package com.sdust.stos.dto;
 
 import com.sdust.stos.entity.DgList;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
 public class DgListDto extends DgList{
 
     private List<DgList> list = new ArrayList<>();
